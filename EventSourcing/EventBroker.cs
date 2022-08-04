5d079c864f5ded98ccbfa5794093fc5dfc7a6b36
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2022-08-04 12:40:12 +0600

    init project and add message broker engine

---
+++ EventSourcing/EventBroker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing
+{
+    public class EventBroker
+    {
+        // 1. Event Storage , Store All Events
+        IList<Event> eventList = new List<Event>();
+        // 2. Commands
+        event EventHandler<Command> Commands;
+        // 3. Queries
+        event EventHandler<Query> Queries;
+
+        void Command(Command c) => Commands?.Invoke(this, c);
+    }
+}
